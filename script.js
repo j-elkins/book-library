cdf5bdc75fe8e34c877207284c6f6c76a6645aec
Jasmine Elkins <jasmine.elkins@gmail.com>
2021-12-20 10:38:46 -0500

    Fixed local storage functions, simplified Class structure

--- script.js
+++ script.js
@@ -11,60 +11,17 @@ let myLibrary = [];
 
 // rewrite using Classes
 class Book {
-  set title(t) {
-    this._title = t;
-  }
-  set author(a) {
-    this._author = a;
-  }
-  set pageCount(p) {
-    this._pageCount = p;
-  }
-  set status(s) {
-    this._status = s;
-  }
-
-  get title() {
-    return this._title;
-  }
-  get author() {
-    return this._author;
-  }
-  get pageCount() {
-    return this._pageCount;
-  }
-  get status() {
-    return this._status;
-  }
-
   constructor(title, author, pageCount, status) {
-    this._title = title;
-    this._author = author;
-    this._pageCount = pageCount;
-    this._status = status;
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.status = status;
+  }
+
+  sayHello() {
+    console.log("My title is " + this.title);
   }
 }
-
-// manually add a few books to get the display working..
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", "read");
-
-const thePaperMenagerie = new Book(
-  "The Paper Menagerie",
-  "Ken Liu",
-  "464",
-  "read"
-);
-
-const aPromisedLand = new Book(
-  "A Promised Land",
-  "Barack Obama",
-  "768",
-  "not read"
-);
-
-// add manual books to library array
-myLibrary.push(theHobbit, thePaperMenagerie, aPromisedLand);
-console.log({ myLibrary });
 
 // loop through myLibrary array and display each book on the page
 const bookTileContainer = document.querySelector("#bookTileContainer");
@@ -72,6 +29,7 @@ const bookTileContainer = document.querySelector("#bookTileContainer");
 function createBookTileWithInfo() {
   bookTileContainer.innerHTML = "";
   myLibrary.forEach((currentBook) => {
+    currentBook.sayHello();
     // create a new book tile populated with book info
     const bookTile = document.createElement("div");
     bookTile.classList.add("tile");
@@ -184,17 +142,49 @@ function saveLibraryToLocalStorage() {
 // function to look for that array when app is first loaded /
 // function to pull books from local storage when page refreshed
 function checkIfLibraryExistsInLocalStorage() {
-  if (!localStorage.myLibrary) {
-    createBookTileWithInfo();
+  const storedLibrary = localStorage.getItem("myLibrary"); // gets information from local storage to use in below loop to create DOM/display
+  if (storedLibrary) {
+    const parsedLibrary = JSON.parse(storedLibrary);
+    myLibrary = parsedLibrary.map(function (almostBook) {
+      // return new Book(
+      //   almostBook.title,
+      //   almostBook.author,
+      //   almostBook.pageCount,
+      //   almostBook.status
+      // );
+
+      Object.setPrototypeOf(almostBook, Book.prototype);
+      return almostBook;
+    });
+    console.log("Library restored");
   } else {
-    let objects = localStorage.getItem("myLibrary"); // gets information from local storage to use in below loop to create DOM/display
-    objects = JSON.parse(objects);
-    myLibrary = objects;
-    createBookTileWithInfo();
+    // manually add a few books to get the display working..
+    const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", "read");
+
+    const thePaperMenagerie = new Book(
+      "The Paper Menagerie",
+      "Ken Liu",
+      "464",
+      "read"
+    );
+
+    const aPromisedLand = new Book(
+      "A Promised Land",
+      "Barack Obama",
+      "768",
+      "not read"
+    );
+
+    // add manual books to library array
+    myLibrary.push(theHobbit, thePaperMenagerie, aPromisedLand);
+
+    console.log("Default library created");
   }
+  createBookTileWithInfo();
 }
+
 // make sure app doesn't crash if lib array isn't in localStorage..
 
-// window.onload = function () {
-//   checkIfLibraryExistsInLocalStorage();
-// };
+window.onload = function () {
+  checkIfLibraryExistsInLocalStorage();
+};
